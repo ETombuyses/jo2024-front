b38fa51a2fdd3461d86ad5fc1aee4578873f0e3a
ebergez <emilebergez@gmail.com>
2020-02-07 00:34:39 +0100

    Integration des filtres

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -1,30 +1,35 @@
-import React from 'react';
-import basketball from '../assets/img/Basketball.svg';
-import Sport from '../Components/Sport';
-import FauteuilRoulant from '../assets/img/FauteuilRoulant.svg';
-import HandicapSensoriel from '../assets/img/HandicapSensoriel.svg'
+import React from "react";
+import basketball from "../assets/img/Basketball.svg";
+import Sport from "../Components/Sport";
+import FauteuilRoulant from "../assets/img/FauteuilRoulant.svg";
+import HandicapSensoriel from "../assets/img/HandicapSensoriel.svg";
 
 class Popup extends React.Component {
-    constructor() {
-      super();
-      this.state = {
-          arrondissement: "1er Arrondissement",
-          Sport: "Basketball",
-          CategorieChoisie: "Sport de ballons :"
-
-
-
-      }
-    }
-    render() {
-      return <div className="popup">
+  constructor() {
+    super();
+    this.state = {
+      arrondissement: "1er Arrondissement",
+      Sport: "Basketball",
+      CategorieChoisie: "Sport de ballons :"
+    };
+  }
+  render() {
+    return (
+      <div className="popup">
         <p className="Arrondissement">{this.state.arrondissement}</p>
         <div className="currentOlympicWrapper">
           <div className="currentOlympic">
-              <img src={basketball} alt="Epreuve en cours" className="currentOlympicSport"/>
+            <img
+              src={basketball}
+              alt="Epreuve en cours"
+              className="currentOlympicSport"
+            />
           </div>
-          <p className="currentOlympicDescription">Epreuve olympique<br/>
-            BasketBall - Olympia de Paris</p>
+          <p className="currentOlympicDescription">
+            Epreuve olympique
+            <br />
+            BasketBall - Olympia de Paris
+          </p>
         </div>
         <div className="buildingsSection">
           <h2 className="buildingsHead">{this.state.CategorieChoisie}</h2>
@@ -36,28 +41,39 @@ class Popup extends React.Component {
           <Sport name="Tennis" amount="8"></Sport>
         </div>
         <div className="filtersSection">
-            <h3>Filtrer les résultats</h3>
-            <div className="filtersSectionWrapper">
-              <div className="accesibilityFilters">
-                <h4>Accéssibilité</h4>
-                <div className="accesibilityFiltersWrapper">
-                  <div>
-                    <img src={FauteuilRoulant} alt="Mobilité Réduite"/>
-                    <h5>Mobilité réduite</h5>
-                  </div>
-                  <div>
-                    <img src={HandicapSensoriel} alt="Handicap Sensoriel"/>
-                    <h5>Handicap Sensoriel</h5>
-                  </div>
+          <h3>Filtrer les résultats</h3>
+          <div className="filtersSectionWrapper">
+            <div className="accesibilityFilters">
+              <h4>Accéssibilité</h4>
+              <div className="accesibilityFiltersWrapper">
+                <div>
+                  <img src={FauteuilRoulant} alt="Mobilité Réduite" />
+                  <h5>Mobilité réduite</h5>
+                </div>
+                <div>
+                  <img src={HandicapSensoriel} alt="Handicap Sensoriel" />
+                  <h5>Handicap Sensoriel</h5>
                 </div>
               </div>
-              <div className="typeOfSportsFilters">
-
+            </div>
+            <div className="typeOfSportsFilters">
+              <h4>Type de pratique</h4>
+              <div className="typesOfSports">
+                <div className="typesOfSportsFirstWrapper">
+                  <button>Loisir</button>
+                  <button>Entrainement</button>
+                </div>
+                <div className="typesOfSportsSecondWrapper">
+                  <button>Compétition</button>
+                  <button>Scolaire</button>
+                </div>
               </div>
             </div>
+          </div>
         </div>
       </div>
-    }
+    );
   }
+}
 
-  export default Popup;
+export default Popup;
