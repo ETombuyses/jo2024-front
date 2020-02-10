712faa0710fb5b98e55078e2ac27da5f1965bc76
ebergez <emilebergez@gmail.com>
2020-02-10 15:33:46 +0100

    added second popup part

---
+++ src/Components/Adress.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Adress extends React.Component{
+  constructor(){
+      super();
+
+  }
+  render() {
+      return (
+        <div className="adressWrapper">
+          <h3>{this.props.name}</h3>
+          <h3>{this.props.adress}</h3>
+        </div>
+      )
+  }
+}
+
+export default Adress;
--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -3,6 +3,7 @@ import basketball from "../assets/img/Basketball.svg";
 import Sport from "../Components/Sport";
 import FauteuilRoulant from "../assets/img/FauteuilRoulant.svg";
 import HandicapSensoriel from "../assets/img/HandicapSensoriel.svg";
+import Adress from "../Components/Adress";
 
 class Popup extends React.Component {
   constructor() {
@@ -11,7 +12,8 @@ class Popup extends React.Component {
       arrondissement: "1er Arrondissement",
       Sport: "Basketball",
       CategorieChoisie: "Sport de ballons :",
-      SportChoisi: false
+      SportChoisi: false,
+      ClickedSport: "Basketball"
     };
   }
 
@@ -23,34 +25,41 @@ class Popup extends React.Component {
   displaySportsOrPlaces() {
     if (this.state.SportChoisi === false) {
       return (
-        <div>
-        <p className="Arrondissement">{this.state.arrondissement}</p>
-        <div className="currentOlympicWrapper">
-          <div className="currentOlympic">
-            <img src={basketball} alt="Epreuve en cours" className="currentOlympicSport"/>
+        <div className="forreturn">
+          <div className="currentOlympicWrapper">
+            <div className="currentOlympic">
+              <img src={basketball} alt="Epreuve en cours" className="currentOlympicSport"/>
+            </div>
+            <p className="currentOlympicDescription">
+              Epreuve olympique
+              <br />
+              BasketBall - Olympia de Paris
+            </p>
           </div>
-          <p className="currentOlympicDescription">
-            Epreuve olympique
-            <br />
-            BasketBall - Olympia de Paris
-          </p>
-        </div>
-        <div className="buildingsSection">
-          <h2 className="buildingsHead">{this.state.CategorieChoisie}</h2>
-          <Sport name="Basketball" amount="40" onClick={()=>{{this.switchDisplay()}}} ></Sport>
-          <Sport name="Football" amount="30" onClick={()=>{{this.switchDisplay()}}}></Sport>
-          <h3 className="otherSportsHead onClick={()=>{{this.switchDisplay()}}}">Autres sports :</h3>
-          <Sport name="Jogging" amount="20" onClick={()=>{{this.switchDisplay()}}}></Sport>
-          <Sport name="Skateboard" amount="48" onClick={()=>{{this.switchDisplay()}}}></Sport>
-          <Sport name="Tennis" amount="8" onClick={()=>{{this.switchDisplay()}}}></Sport>
-          <Sport name="Saut en longueur" amount="1" onClick={()=>{{this.switchDisplay()}}}></Sport>
-        </div>
+          <div className="buildingsSection">
+            <h2 className="buildingsHead">{this.state.CategorieChoisie}</h2>
+            <Sport name="Basketball" amount="40" onClick={()=>{{this.switchDisplay()}}} ></Sport>
+            <Sport name="Football" amount="30" onClick={()=>{{this.switchDisplay()}}}></Sport>
+            <h3 className="otherSportsHead">Autres sports :</h3>
+            <Sport name="Jogging" amount="20" onClick={()=>{{this.switchDisplay()}}}></Sport>
+            <Sport name="Skateboard" amount="48" onClick={()=>{{this.switchDisplay()}}}></Sport>
+            <Sport name="Tennis" amount="8" onClick={()=>{{this.switchDisplay()}}}></Sport>
+            <Sport name="Saut en longueur" amount="1" onClick={()=>{{this.switchDisplay()}}}></Sport>
+          </div>
         </div>
       );
     } else {
       return (
-        <div className="buildingsSection">
-          
+        <div className="forreturn">
+          <div className="selectedSport">
+            <img src={basketball} alt=""/>
+            <h3>{this.state.ClickedSport}</h3>
+          </div>
+          <h2 className="buildingsHead">Liste des établissements :</h2>
+          <div className="buildingsSection">
+            <Adress name="Gymnase du pérou" adress="78 rue Saint-Denis"></Adress>
+            <Adress name="Gymnase du pérou" adress="78 rue Saint-Denis"></Adress>
+          </div>
         </div>
       )
     }
@@ -59,6 +68,8 @@ class Popup extends React.Component {
   render() {
     return (
       <div className="popup">
+        <p className="Arrondissement">{this.state.arrondissement}</p>
+
         {this.displaySportsOrPlaces()}
 
         <div className="filtersSection">
