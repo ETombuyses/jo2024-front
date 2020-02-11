a8edb1038300df2aa9d0d9352f6d60016631ca2e
ebergez <emilebergez@gmail.com>
2020-02-11 01:02:37 +0100

    slight fix to filters

--- src/Components/Popup.jsx
+++ src/Components/Popup.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import basketball from "../assets/img/Basketball.svg";
 import Sport from "../Components/Sport";
-import FauteuilRoulant from "../assets/img/FauteuilRoulant.svg";
-import HandicapSensoriel from "../assets/img/HandicapSensoriel.svg";
+
 import Adress from "../Components/Adress";
+import Filters from "../Components/Filters";
 
 class Popup extends React.Component {
   constructor() {
@@ -76,39 +76,7 @@ class Popup extends React.Component {
 
         {this.displaySportsOrPlaces()}
 
-        <div className="filtersSection">
-          <h3>Filtrer les résultats</h3>
-          <div className="filterSectionWrapperWrapper">
-          <div className="filtersSectionWrapper">
-            <div className="accesibilityFilters">
-              <h4>Accéssibilité</h4>
-              <div className="accesibilityFiltersWrapper">
-                <div>
-                  <img src={FauteuilRoulant} alt="Mobilité Réduite" />
-                  <h5>Mobilité réduite</h5>
-                </div>
-                <div>
-                  <img src={HandicapSensoriel} alt="Handicap Sensoriel" />
-                  <h5>Handicap Sensoriel</h5>
-                </div>
-              </div>
-            </div>
-            <div className="typeOfSportsFilters">
-              <h4>Type de pratique</h4>
-              <div className="typesOfSports">
-                <div className="typesOfSportsFirstWrapper">
-                  <button>Loisir</button>
-                  <button>Entrainement</button>
-                </div>
-                <div className="typesOfSportsSecondWrapper">
-                  <button>Compétition</button>
-                  <button>Scolaire</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          </div>
-        </div>
+        <Filters></Filters>
       </div>
     );
   }
